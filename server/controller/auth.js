b7805e15073ebd956e55d0a23b253e3b8060210e
saeha303 <sumaiyasaeha@gmail.com>
2024-06-08 17:43:07 +0600

    18

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -34,7 +34,7 @@ exports.signup = async (req, res) => {
     }
 };
 
-const signin= exports.signin = (req, res) => {
+exports.signin = (req, res) => {
     // console.log(req.body);
     const errors = validationResult(req);
     // console.log(errors);
@@ -124,27 +124,3 @@ exports.isAdmin = (req, res, next) => {
     next();
 };
 
-const allowCors = fn => async (req, res) => {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-    if (req.method === 'OPTIONS') {
-      res.status(200).end()
-      return
-    }
-    return await fn(req, res)
-  }
-  
-  const handler = (req, res) => {
-    const d = new Date()
-    res.end(d.toString())
-  }
-  
-  module.exports = allowCors(signin)
-  
