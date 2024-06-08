2219cad3ae55a192ca45640f67cb45729f8f817e
saeha303 <sumaiyasaeha@gmail.com>
2024-06-08 19:08:57 +0600

    26

--- server/index.js
+++ server/index.js
@@ -15,15 +15,15 @@ app.use(cors(
 ));
 app.use(cookieParser());
 app.use(express.json())
-// app.use((req, res, next) => {
-//   res.header(
-//     "Access-Control-Allow-Origin",
-//     "https://plenty-front.vercel.app"
-//   );
-//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//   res.header("Access-Control-Allow-Headers", "Content-Type");
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://plenty-front.vercel.app"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 app.listen(process.env.PORT, () => {
   console.log(`Server listening on ${process.env.PORT}`);
 })
@@ -74,3 +74,27 @@ app.use("/api", sellRoutes);
 //   res.set('Access-Control-Allow-Origin', '*');
 //   res.send({ "msg": "This has CORS enabled 🎈" })
 //   })
+
+// "headers": [
+//         {
+//           "source": "/api/(.*)",
+//           "headers": [
+//             {
+//               "key": "Access-Control-Allow-Credentials",
+//               "value": "true"
+//             },
+//             {
+//               "key": "Access-Control-Allow-Origin",
+//               "value": "*"
+//             },
+//             {
+//               "key": "Access-Control-Allow-Methods",
+//               "value": "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+//             },
+//             {
+//               "key": "Access-Control-Allow-Headers",
+//               "value": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+//             }
+//           ]
+//         }
+//       ]
