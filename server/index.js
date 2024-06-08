034efe19fcf064b905784552227969b148d53b0c
saeha303 <sumaiyasaeha@gmail.com>
2024-06-08 18:33:33 +0600

    23

--- server/index.js
+++ server/index.js
@@ -6,13 +6,24 @@ var app = express();
 
 app.use(cors(
 {
-  // origin:["https://plenty-ten.vercel.app"],
-  // methods:["GET","POST","PUT","DELETE","OPTIONS"]
+  origin:'https://plenty-ten.vercel.app',
+  methods:["GET","POST","PUT","DELETE","OPTIONS"],
+  allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
+  credentials: true
 }
 
 ));
 app.use(cookieParser());
-
+app.use(express.json())
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://plenty-front.vercel.app"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 app.listen(process.env.PORT, () => {
   console.log(`Server listening on ${process.env.PORT}`);
 })
