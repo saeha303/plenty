51432ae9c4b91b984610c4088850f60863840541
saeha303 <sumaiyasaeha@gmail.com>
2024-06-08 18:49:48 +0600

    25

--- server/index.js
+++ server/index.js
@@ -5,25 +5,25 @@ const cookieParser = require('cookie-parser');
 var app = express();
 
 app.use(cors(
-{
-  origin:'*',
-  methods:["GET","POST","PUT","DELETE","OPTIONS"],
-  allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
-  credentials: true
-}
+// {
+//   origin:'*',
+//   methods:["GET","POST","PUT","DELETE","OPTIONS"],
+//   allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
+//   credentials: true
+// }
 
 ));
 app.use(cookieParser());
 app.use(express.json())
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://plenty-front.vercel.app"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header(
+//     "Access-Control-Allow-Origin",
+//     "https://plenty-front.vercel.app"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.header("Access-Control-Allow-Headers", "Content-Type");
+//   next();
+// });
 app.listen(process.env.PORT, () => {
   console.log(`Server listening on ${process.env.PORT}`);
 })
@@ -70,7 +70,7 @@ app.use("/api", notificationRoutes);
 const sellRoutes = require("./routes/sell");
 app.use("/api", sellRoutes);
 
-app.get('/cors', (req, res) => {
-  res.set('Access-Control-Allow-Origin', '*');
-  res.send({ "msg": "This has CORS enabled 🎈" })
-  })
+// app.get('/cors', (req, res) => {
+//   res.set('Access-Control-Allow-Origin', '*');
+//   res.send({ "msg": "This has CORS enabled 🎈" })
+//   })
